1109af372fd2e80f695b4137b06b73be94a5e23b
charles.chandler <charles.chandler@onit.com>
2020-11-28 16:08:22 -0600

    adding keyboard event listener

--- drum_kit_starting_files/index.js
+++ drum_kit_starting_files/index.js
@@ -1,10 +1,56 @@
+function playSound (key) {
+  switch (key) {
+    case "w":
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    
+    case "a":
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "s":
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "d":
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "j":
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "k":
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    case "l":
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    default:
+      console.log(triggerInnerHTML);
+
+  }
+}
 
 document.querySelectorAll(".drum").forEach(item => {
   item.addEventListener("click", function () {
 
-    this.style.color="white";
+    triggerInnerHTML = this.innerHTML
+    playSound(triggerInnerHTML)
+    
 
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
   });
+});
+
+document.addEventListener("keydown", function(event) {
+  playSound(event.key)
 });
