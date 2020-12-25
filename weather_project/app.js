6f955f5f279b4876153b2704e5cddbef7d086456
Charles Chandler <charles@boundshollow.com>
2020-12-24 20:45:10 -0500

    adding the ability to name the location you get weather for

--- weather_project/app.js
+++ weather_project/app.js
@@ -1,11 +1,45 @@
 /* eslint-disable no-console */
 const express = require('express');
 const https = require('https');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  const weatherAPI = 'https://api.openweathermap.org/data/2.5/weather?appid=d0b7df5b8a7c1a66d06f26c12b56f258&zip=37934&units=imperial';
+ res.sendFile(`${__dirname}/index.html`);
+});
+
+app.listen(3145, () => {
+  console.log('server is running on port 3145');
+
+});
+
+app.post('/', (req, res) => {
+  console.log('Post received');
+  console.log(req.body.locMethod);
+  console.log(req.body.locator);
+  const { locMethod, locator} = req.body;
+  getWeather(res, locMethod, locator, 'imperial');
+});
+
+function getWeather(res, locMethod, locator, units) {
+  const apiKey = 'd0b7df5b8a7c1a66d06f26c12b56f258';
+
+  let unitSymbol;
+  switch(units) {
+    case 'imperial':
+      unitSymbol = '&#8457;';
+      break;
+    case 'metric':
+      unitSymbol = '&#8451;';
+      break;
+    default:
+      unitSymbol = 'K';
+  }
+
+  const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&${locMethod}=${locator}&units=${units}`;
   https.get(weatherAPI, (response) => {
     console.log(response.statusCode);
 
@@ -15,7 +49,7 @@ app.get('/', (req, res) => {
       const { description, icon } = weatherData.weather[0];
       console.log(`${temp} ${feelsLike} ${description}`);
       res.write('<h1>');
-      res.write(`The temperature in ${weatherData.name} is ${temp}&#8457;. `, 'utf8');
+      res.write(`The temperature in ${weatherData.name} is ${temp}${unitSymbol}. `, 'utf8');
       res.write('</h1>');
       res.write('<p>');
       res.write(`The weather outside is ${description}. `);
@@ -24,8 +58,4 @@ app.get('/', (req, res) => {
       res.send();
     });
   });
-});
-
-app.listen(3145, () => {
-  console.log('server is running on port 3145');
-});
+}
