3cb55f28750eacc995a0b12a7c64691a1830b3a2
chandlercv <chandlercv@yahoo.com>
2021-01-02 21:50:04 -0500

    Complete Challenge 12; send posts to home page; console log posts from home page

--- ejs_challenge/app.js
+++ ejs_challenge/app.js
@@ -20,8 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home', { homeStartingText: homeStartingContent });
-  console.log(posts);
+  res.render('home', { homeStartingText: homeStartingContent, posts });
 });
 
 app.get('/about', (req, res) => {
