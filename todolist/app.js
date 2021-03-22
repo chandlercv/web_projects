1c09c172967f5540be234c3ece2d5a262ec350f5
Charles Chandler <charles@boundshollow.com>
2021-03-21 22:12:19 -0400

    Move list from memory to mongo; Add mongoose dependency

--- todolist/app.js
+++ todolist/app.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-console */
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // using . instead of __dirname here because when . is used in a call to require,
 //  it's evaluted relative the file containing the require statement.
@@ -11,9 +13,6 @@ const cal = require('./date.js');
 
 const port = 3145;
 
-const items = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workItems = [];
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -21,31 +20,65 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  const currentDay = cal.getDay();
-  res.render('list', { listTitle: currentDay, listItems: items });
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
 });
 
-app.post('/', (req, res) => {
-  const { newItem, list } = req.body;
+const Item = mongoose.model('Item', itemsSchema);
 
-  if (list === 'Work Items') {
-    workItems.push(newItem);
-    res.redirect('/work');
-  } else {
-    items.push(newItem);
-    res.redirect('/');
-  }
-});
+const welcome = new Item({ name: 'Welcome to your todolist!' });
+const addItem = new Item({ name: 'Hit the + button to add a new item' });
+const deleteItem = new Item({ name: '<-- Hit this to delete an item' });
+const defaultItems = [welcome, addItem, deleteItem];
 
-app.get('/work', (req, res) => {
-  res.render('list', { listTitle: 'Work Items', listItems: workItems });
-});
+mongoose.connect('mongodb://localhost:27017/todolistDB', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => Item.find({}))
+  .then((list) => list.forEach((listItem) => console.log(listItem.name)))
+  .then(() => {
+    app.get('/', (req, res) => {
+      const currentDay = cal.getDay();
+      Item.find({})
+        .then((items) => {
+          if (items.length === 0) {
+            return Item.insertMany(defaultItems)
+              .then((insertRes) => {
+                console.log(insertRes);
+                return Item.find({});
+              })
+              .then((newItems) => newItems);
+          }
+          return items;
+        })
+        .then((retItems) => res.render('list', { listTitle: currentDay, listItems: retItems }));
+    });
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
+    app.post('/', (req, res) => {
+      const { newItem, list } = req.body;
 
-app.listen(port, () => {
-  console.log(`Server started on ${port}`);
-});
+      if (list === 'Work Items') {
+        workItems.push(newItem);
+        res.redirect('/work');
+      } else {
+        items.push(newItem);
+        res.redirect('/');
+      }
+    });
+
+    app.get('/work', (req, res) => {
+      res.render('list', { listTitle: 'Work Items', listItems: workItems });
+    });
+
+    app.get('/about', (req, res) => {
+      res.render('about');
+    });
+
+    return app.listen(port, () => {
+      console.log(`Server started on ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
